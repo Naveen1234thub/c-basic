9d9292f1ee111277fdda37c0b6b5d4c95dfb6abc
naveen <92910170+Naveen1234thub@users.noreply.github.com>
2021-10-27 23:55:43 -0700

    Add files via upload

---
+++ intro of c-basic.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+	printf("ESCAPE CODES\n1\\n-use as enter to crusher down.\n2.\\t-name kindha name ravdaniki ki\n3.\\r-carry return to bring the crusher to begning");
+	printf("\n3.\\""rich dad book is written by \"robert\"");
+}
