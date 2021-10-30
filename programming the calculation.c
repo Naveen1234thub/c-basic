f15bc2a7d9ad8b71bfa44f92625ad04eb8d8e2fd
naveen <92910170+Naveen1234thub@users.noreply.github.com>
2021-10-29 23:56:44 -0700

    Add files via upload

---
+++ programming the calculation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	//variables
+	int num1,num2,num3,num4,sum;
+	//intialization
+	num1=16;
+	num2=565;
+	num3=65;
+	num4=654;
+	//calculation
+	sum = num1+num2+num3+num4;
+	//output
+	printf("sum of %d,%d,%d and %d is %d", num1,num2,num3,num4,sumSSVS);
+}
---
+++ programming with  (sum of 4 numbers)short cut.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	//variables
+	float num1=65.5,num2=54.9,multi=num1*num2;
+
+
+	//output
+	printf("sum of %f, and %f is %f", num1,num2,multi);
+}
